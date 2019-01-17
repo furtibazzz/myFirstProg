0d7902faf980b38c62b7ffa396c7d828d1568f69
furtibazzz <41450259+furtibazzz@users.noreply.github.com>
2019-01-17 16:00:45 +0530

    First Way to insert element twaice in a list.

--- Py_append.py
+++ Py_append.py
@@ -1,3 +1,8 @@
 #inserting a comment line...
+def append_twice(list_name,value):
+  list_name.append(value)
+  list_name.append(value)
+  
 nums = [1,2,3]
-append_twice(nums,7) #[1,2,3,7,7]
+append_twice(nums,7) 
+print(nums)  #[1,2,3,7,7]
